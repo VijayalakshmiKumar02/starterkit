6095612ada34b5cd7a45a54ec574a4ba0f0cb960
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-01-04 18:07:19 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 set -e -x
-
+echo "making directory"
 mkdir resource-app
-
+echo "changing directory"
 cd resource-app
-
+echo "git clone"
 git clone starterkit resource-app
 
 #mvn clean compile
