689fe236ee6468f9c9233a4e39aa466319bcb1e6
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-01-05 16:56:06 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 set -e -x
+vagrant ssh
+mkdir home/vagrant/test
+cd home/vagrant/test
+var=$(pwd)
+echo "The current working directory $var."
 echo "making directory"
-mkdir resource-app1
 echo "changing directory"
 #cd resource-app1
 echo "git clone"
-git clone starterkit resource-app1
-cd resource-app1
-mvn clean compile
-mvn package
-
+git clone starterkit home/vagrant/test
+#cd resource-app1
+mvn install -DskipTests=true
+#mvn package
+cp 
 #mvn install
 #mvn package
-find .
+#find .
 #java de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication
 #mvn test
 echo "done testing"
