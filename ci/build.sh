21ac3d83a58cfd2dd91a5015a28804d7ea9f83bf
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-01-05 18:56:24 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 set -e -x
-mkdir jarfolder
-cd jarfolder
-var=$(pwd)
-echo "The current working directory $var."
+#mkdir jarfolder
+#cd jarfolder
+#echo "The current working directory $var."
 echo "making directory"
 echo "changing directory"
 #cd resource-app1
 echo "git clone"
-git clone starterkit var
+git clone starterkit resource-app
 mvn install -DskipTests=true
 #mvn package
 #cp ./target/DeployProcessorSupportTool.jar home/vagrant/test
