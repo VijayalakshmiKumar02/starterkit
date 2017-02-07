2145f045be4bb06fc059ae269da841a6db6b9a1a
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-02-07 12:25:54 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -16,10 +16,10 @@ cd resource-app
 #done
 #echo "done"
 #mvn sonar:sonar
-mvn sonar:sonar -Dsonar.host.url=https://sonarqube.mvp2.cognizantone.org/
+mvn sonar:sonar \
+    -Dsonar.host.url=https://sonarqube.mvp2.cognizantone.org/
 #mvn sonar:sonar -Dsonar.host.url=https://sonarqube.mvp2.cognizantone.org
- mvn sonar-scanner
-#var=$(pwd)
+
 #echo "The current working directory $var."
 #cd target
 #var1=$(pwd)
